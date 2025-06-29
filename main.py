19d8fe351d88862e3eea4f9811696a5c0b4a7e73
Fazilat Jahan <160049943+Fazilat-Jahan@users.noreply.github.com>
2025-06-29 16:21:05 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+print("""
+
+Oh hey! I caught you bro... hahaha 👀  
+You really cloned it, didn’t you?  
+Didn’t even hesitate, huh?
+
+Looking for the next million-dollar startup idea?  
+Sorry bro — just a gentle reminder:
+
+👋 If you're gonna take the repo,  
+at least take a second to follow the human behind it 💚
+
+No stars, no follow...  
+but still had time to clone?  
+That’s wild 😭
+
+One click won’t hurt —  
+but ignoring devs who create stuff? That’s cold 💔
+
+– With love,  
+  The repo you just cloned.
+""")
